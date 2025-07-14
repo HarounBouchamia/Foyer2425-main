7a3646ddcb85af401dcd42005c2474918b4cf22a
Haroun Bouchamia <harounbouchamia123@gmail.com>
2025-07-15 00:46:54 +0100

    tes

--- src/test/java/tn/esprit/spring/BlocServiceTest.java
+++ src/test/java/tn/esprit/spring/BlocServiceTest.java
@@ -1,13 +1,34 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BlocServiceTest {
 
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all tests");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all tests");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test");
+    }
+
     @Test
-    void simpleTest() {
-        Assertions.assertTrue(true); // Simple dummy test
+    @Order(1)
+    void test1() {
+        Assertions.assertTrue(true);
     }
 }
+
